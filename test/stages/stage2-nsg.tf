17f251e08b6dfe76037f193e4a508a35ef4311bd
Rich Ehrhardt <51330294+rich-ehrhardt@users.noreply.github.com>
2022-08-25 15:28:35 +1000

    Initial version (#2)
    * Initial version
    
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Changed terraform version
    
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Fixeded region value to test stage
    
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Fixed subnet names in nsg module
    
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

---
+++ test/stages/stage2-nsg.tf
@@ -0,0 +1,21 @@
+module "nsg" {
+  source = "./module"
+
+  name_prefix           = "${var.resource_group_name}-${random_string.cluster_id.result}"
+  resource_group_name   = module.resource_group.name
+  virtual_network_name  = module.vnet.name
+  region                = module.resource_group.region
+  subnets               = [module.subnets.names[0], module.subnets.names[1]]
+ 
+  acl_rules = [{
+    name                = "ssh-inbound"
+    priority            = "101"
+    access              = "Allow"
+    protocol            = "Tcp"
+    direction           = "Inbound"
+    source_addr         = "*"
+    destination_addr    = "*"
+    source_ports        = "*"
+    destination_ports   = "22"
+  }]
+}
